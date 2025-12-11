06ed463d56e5eb0eb039af7e4bc9ef21ae6325f6
Bonifac0 <kolenomi@seznam.cz>
2025-12-11 13:44:09 +0100

    cislicka

---
+++ lua/custom/init.lua
@@ -0,0 +1,8 @@
+-- Search settings
+vim.opt.hlsearch = true -- highlight all matches
+vim.opt.ignorecase = true -- case-insensitive search...
+vim.opt.smartcase = true -- ...unless search pattern has uppercase
+
+vim.opt.tabstop = 4 -- number of visual spaces per TAB
+vim.opt.shiftwidth = 4 -- spaces inserted for each indentation
+vim.opt.expandtab = true -- press TAB to insert spaces
