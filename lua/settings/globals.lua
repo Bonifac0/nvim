5bb1b2e32795372cfd487e70503bb249eb8ff11f
Bonifac0 <kolenomi@seznam.cz>
2026-04-01 14:34:55 +0200

    move settings to its files

---
+++ lua/settings/globals.lua
@@ -0,0 +1,11 @@
+-- vim.g -> global variables (for plugins & config)
+-- This sets global variables, often used by plugins.
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
