cbf9fbd020ab723acb8f23590c1e699a40b339dd
Bonifac <kolenomi@seznam.cz>
2026-06-24 18:55:06 +0200

    add python support

--- init.lua
+++ init.lua
@@ -694,7 +694,8 @@ do
   local servers = {
     -- clangd = {},
     -- gopls = {},
-    -- pyright = {},
+    pyright = {},
+    ruff = {},
     -- rust_analyzer = {},
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -799,6 +800,7 @@ do
       -- rust = { 'rustfmt' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
+      python = {"ruff"},
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
